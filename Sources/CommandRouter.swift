4c6fa5dd45bef4c673c8fa9b8288c0200726d3f6
Vadym Markov <markov.vadym@gmail.com>
2016-09-05 00:13:29 +0200

    Introduce command router

--- Sources/CommandRouter.swift
+++ Sources/CommandRouter.swift
@@ -1,9 +1,35 @@
-//
-//  CommandRouter.swift
-//  AftermathCompass
-//
-//  Created by Vadym Markov on 05/09/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
+import Aftermath
+import Compass
 
-import Foundation
+// MARK: - Command route
+
+public protocol CommandRoute {
+  func buildCommand(from location: Location) throws -> AnyCommand
+}
+
+// MARK: - Command router
+
+public struct CommandRouter: CommandProducer {
+  public var routes = [String : CommandRoute]()
+  public init() {}
+
+  public func execute(location: Location) -> Bool {
+    guard let route = routes[location.path] else {
+      return false
+    }
+
+    execute(RouteCommandBuilder(route: route, location: location))
+    return true
+  }
+}
+
+// MARK: - Route command builder
+
+struct RouteCommandBuilder: CommandBuilder {
+  let route: CommandRoute
+  let location: Location
+
+  func buildCommand() throws -> AnyCommand {
+    return try route.buildCommand(from: location)
+  }
+}
