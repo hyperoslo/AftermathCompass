89893037e7813dfdcb8cc59aa63b7b24a68f7f50
Vadym Markov <markov.vadym@gmail.com>
2016-09-05 00:13:17 +0200

    Carthage update

---
+++ Sources/CommandRouter.swift
@@ -0,0 +1,9 @@
+//
+//  CommandRouter.swift
+//  AftermathCompass
+//
+//  Created by Vadym Markov on 05/09/16.
+//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
